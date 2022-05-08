f2450c53750b197942d29590a917532dd0de82fd
Buzz Novik <94489809+bm-novik@users.noreply.github.com>
2022-05-08 15:05:46 +0300

    hi
    Signed-off-by: Buzz Novik <94489809+bm-novik@users.noreply.github.com>

---
+++ src/pages/Test.js
@@ -0,0 +1,15 @@
+// import {ConfirmUnfollow} from "../components/modalPages/ConfirmUnfollow";
+// import {useState} from 'react'
+//
+// export const Test = () => {
+//     const [openConfirmModal, setOpenConfirmModal] = useState(true)
+//     const [confirm, setConfirm] = useState(false)
+//
+//     const handleModalClick = () => {setOpen(!open)}
+//
+//     return (
+//         <div>
+//             <ConfirmUnfollow open={open} handleModalClick={handleModalClick} setConfirm={setConfirm} />
+//         </div>
+//     );
+// };
