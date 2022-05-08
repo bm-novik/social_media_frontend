acf1666cb8a13c5ef27c6ece97d308e7b99fbc96
Buzz Novik <94489809+bm-novik@users.noreply.github.com>
2022-05-08 16:06:08 +0300

    hi1
    Signed-off-by: Buzz Novik <94489809+bm-novik@users.noreply.github.com>

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:8000"
+// export const BASE_URL = "http://127.0.0.1:8000"
+export const BASE_URL = "ec2-44-198-56-21.compute-1.amazonaws.com/api"
 export const SIGNUP_PIC_ROOT = "/appPic/login.jpg"
 export const LOGIN_PIC_ROOT = "/appPic/login.jpg"
