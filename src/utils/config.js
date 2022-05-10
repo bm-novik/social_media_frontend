3dc020fbdcef4b46e6362246fc0968e6c977659d
Buzz Novik <94489809+bm-novik@users.noreply.github.com>
2022-05-10 12:34:35 +0300

    09.05

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://127.0.0.1:8000/api"
-export const BASE_URL = "/api"
+export const BASE_URL = "http://44.198.56.21/api"
+
 export const SIGNUP_PIC_ROOT = "/appPic/login.jpg"
 export const LOGIN_PIC_ROOT = "/appPic/login.jpg"
